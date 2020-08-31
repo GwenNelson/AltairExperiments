5ea5ecc8e6ffb4366ced472cbc307f97d58c5b5a
Gwen Nelson <gwen@gwennelson.co.uk>
2020-08-31 06:47:33 +0100

    more advanced UART CLI commands

--- src/uart-cli-cmds.asm
+++ src/uart-cli-cmds.asm
@@ -15,80 +15,102 @@
 		mvi a,15h
 		out 10h
 
-; see https://deramp.com/downloads/altair/software/utilities/other/ECHO.ASM
 prompt:		lxi b, promptStr	; output prompt character
 		call writeStr
 
-pollRcv:	in 10h		; poll for received character 
+		call getStr		; read in string into inputBuf
+
+		; check for commands
+		lxi b,inputBuf		; help command first
+		lxi d,helpCmdStr
+		call strcmp
+		jz helpCmd
+
+		lxi b,inputBuf		; then greet command
+		lxi d,greetCmdStr
+		call strcmp
+		jz greetCmd
+
+		jmp invalidCmd		; if we get here, invalid command
+
+
+; =============================================================================
+;  getstr routine
+;   reads a string from the user into inputBuf
+; =============================================================================
+getStr:	
+		; reset inputBufCurPos
+		lxi h,inputBuf
+		shld inputBufCurPos
+getStrLoop:
+		in 10h		; poll for received character
 		rrc		; received character flag in LSB
-		jnc pollRcv	; if not carried, keep polling
+		jnc getStrLoop	; if not carried, keep polling
 		in 11h		; read character in
 
-		cpi 1Bh		; check if the character is an escape code
-		jz  escHandle	; if it is, call the escape character handler
-	
 		cpi 0Dh		; check if the character is enter
-		jz entHandle	; if it is, call the enter character handler
+		jz getStrEnter	; if it is, call enter handler
 
 		; if not a special character, store it at inputBufCurPos
-		lhld inputBufCurPos ; load indirect, H:L now stores address pointed at by inputBufCurPos
-		xchg                ; swap H:L and D:E, D:E now has the address
-		stax d              ; store indirect, A is stored at the address
+		lhld inputBufCurPos 	; load indirect into H:L
+		xchg			; D:E now has address
+		stax d			; store indirect, A stored at address
 
 		; increment buffer pointer
 		lxi h,inputBufCurPos	; set H:L to inputBufCurPos
-		inr m			; increment current cursor position by incrementing that memory location
+		inr m			; increment buffer position
 
-		out 11h		; finally, echo the character
-		jmp pollRcv	; and repeat the polling loop
-
-
-escHandle:
-		jmp pollRcv ; placeholder for now
-
-; =======================================================================
-;  entHandle routine
-;   handles pressing enter - for now simply outputs "You entered:" and
-;   resets inputBuf
-; =======================================================================
-
-entHandle:
-		; first terminate the string
+		out 11h			; echo the character
+		jmp getStrLoop		; repeat polling loop
+getStrEnter:
+		; terminate the string
 		lhld inputBufCurPos 	; load H:L indirect with inputBufCurPos
-		xchg			; D:E is now the actual address pointed to by inputBufCurPos
+		xchg			; D:E is now the actual address
 		xra a			; XOR A with A, sets it to 0
 		stax d			; store A into address at D:E
 
 		lxi b,nlStr		; write a newline
 		call writeStr
 
-		; compare start of string with help command
-		lxi b, inputBuf
-		lxi d, helpCmdStr
-		call strcmp
-
-		; if not equal, call invalidCmd
-		jnz invalidCmd
-		; if equal, call helpCmd
-		jz helpCmd
+		; return to caller
+		ret
+; ===============================================================================
 
 
-endHandleCmd:
-		; now reset inputBufCurPos
-		lxi h, inputBuf		; load H:L with address of inputBuf
-		shld inputBufCurPos	; store H:L into inputBufCurPos
 
-		jmp prompt ; jump back to main loop
+; ======================================================================
+;  command routines
+;  these all jump back to prompt when done
+; ======================================================================
 
 invalidCmd:
 		lxi b,invalidCmdStr
 		call writeStr
-		jmp endHandleCmd
+		jmp prompt
 
 helpCmd:
 		lxi b,helpStr
 		call writeStr
-		jmp endHandleCmd
+		jmp prompt
+
+greetCmd:
+		lxi b,greetPromptStr 	; ask for name
+		call writeStr
+
+		call getStr    		; read name
+
+		lxi b,greetStr		; greet the user
+		call writeStr
+		lxi b,inputBuf
+		call writeStr
+		lxi b,nlStr
+		call writeStr
+
+		jmp prompt
+; ======================================================================
+
+
+
 
 ; ======================================================================
 ;  writeStr routine
@@ -135,11 +157,15 @@ promptStr:	db "> ",0
 nlStr:		db 0Dh,0Ah,0
 
 helpCmdStr:	db "help",0
+greetCmdStr:	db "greet",0
 
 invalidCmdStr:	db "Invalid command!",0Dh,0Ah,0
 
+greetPromptStr:	db "Please enter your name: ",0
+greetStr:	db "Hello ",0
 helpStr:	db "Valid commands are ",0Dh,0AH
-		db "  [help] - display this list",0Dh,0Ah,0
+		db "  [help]  - display this list",0Dh,0Ah,
+		db "  [greet] - runs a simple greeting demo",0Dh,0Ah,0
 
 
 inputBufCurPos: dw inputBuf
