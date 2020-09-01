0b65aecdcc646b26d0decc668f4e8faf4cf2e9ba
Gwen Nelson <gwen@gwennelson.co.uk>
2020-09-01 16:32:08 +0100

    hex output in printf

--- c/src/main.c
+++ c/src/main.c
@@ -3,4 +3,5 @@ extern void printf(const char* s,...) __stdc __z88dk_callee;
 void main() {
 	printf("Hello world\n\r");
 	printf("Testing: %s\n\r","YAY!");
+	printf("Hex: 0x%x\n\r",0xDEAD);
 }
--- c/src/printf.asm
+++ c/src/printf.asm
@@ -25,8 +25,16 @@ _printf:
 	nested: ; this is used to display a string via a nested call
 		inx b   		; increment to the next char
 		ldax b  		; load it
+
 		cpi 's' 		; check if it's a string
-		jnz continue_write	; if not, go back to main loop
+		jz nested_str		; if yes, start handling string
+
+		cpi 'x'			; check if it's a hex value
+		jz nested_hex		; if yes, start handling hex
+
+		jmp continue_write	; else, go back to main loop
+
+	nested_str:
 		inx b			; skip over the 's' character
 
 		pop d ; pop the nested param
@@ -36,6 +44,18 @@ _printf:
 		call _puts
 		pop h ; restore h
 		pop b ; restore b
+		jmp writeStr
+
+	nested_hex:
+		inx b	; skip over the 'x' character
+		
+		pop d   ; pop nested param
+		push b  ; preserve b
+		push h  ; preserve h
+		push d  ; push nested param back to the stack
+		call _puts_hex
+		pop h
+		pop b
 		jmp writeStr
 
 	ret_printf:
@@ -55,4 +75,58 @@ _puts:
 	ret_putf:
 		push h
 		ret
-	
+
+_puts_hex:
+	pop b
+	pop h	
+	mov a,h
+        call writeHexByte
+        mov a,l
+        call writeHexByte
+	push b
+	ret
+
+
+
+; =============================================================================
+;  writeHexNibble
+;   writes a nibble from A to the UART
+; =============================================================================
+writeHexNibble:
+        cpi 10                  ; compare with 10
+        jc writeHexNibbleDigit  ; if <10, write the digit
+
+        sui 10                  ; otherwise, subtract 10 and add 'A'
+        adi 65                  ; add 'A'
+        out 11h                 ; write to the UART
+        ret
+        
+writeHexNibbleDigit:
+        adi 48                  ; add 48 to get ASCII character code 
+        out 11h                 ; write to the UART
+        ret
+
+; =============================================================================
+
+
+; =============================================================================
+;  writeHexByte
+;   writes a byte from A to the UART
+; =============================================================================
+writeHexByte:
+	push b
+        mov b,a                 ; store A in b
+        rrc                     ; rotate right 4 times to get high nibble
+        rrc
+        rrc
+        rrc
+        ani 15                  ; mask it so we only have the rightmost 4 bits
+        call writeHexNibble     ; write the high nibble
+
+        mov a,b                 ; restore A
+        ani 15                  ; mask it
+        call writeHexNibble     ; write the low nibble
+	pop b
+        ret
+; =============================================================================
+
