15b7f9bc20ffcf9159f902e20603f4cb0c8da8d1
Gwen Nelson <gwen@gwennelson.co.uk>
2020-09-01 16:22:12 +0100

    split out nested %s into puts

--- c/src/printf.asm
+++ c/src/printf.asm
@@ -2,7 +2,9 @@
 
 	XLIB _printf
 
-._printf
+; uses __stdc __z88dk_callee convention
+
+_printf:
 	pop h  ; pop return address
 	pop b  ; pop string param
 	writeStr:
@@ -11,26 +13,46 @@
                 cpi 0  ; if end of string, return
                 jz ret_printf
 
-		cpi '%'
+		cpi '%' ; handle % characters
 		jz nested
-                
+
+	continue_write:                
                 out 11h ; write the character
                 
                 inx b   ; increment and loop
                 jmp writeStr
 	
-	nested:
-		inx b ; increment to the next char
-		inx b
+	nested: ; this is used to display a string via a nested call
+		inx b   		; increment to the next char
+		ldax b  		; load it
+		cpi 's' 		; check if it's a string
+		jnz continue_write	; if not, go back to main loop
+		inx b			; skip over the 's' character
+
 		pop d ; pop the nested param
 		push b ; preserve b
 		push h ; preserve h
-		push d
-		call _printf
+		push d ; push the nested param back to the stack
+		call _puts
 		pop h ; restore h
 		pop b ; restore b
 		jmp writeStr
 
 	ret_printf:
+		push h ; restore return address
+		ret
+
+_puts:
+	pop h
+	pop b
+	puts_loop:
+		ldax b
+		cpi 0
+		jz ret_putf
+		out 11h
+		inx b
+		jmp puts_loop
+	ret_putf:
 		push h
 		ret
+	
